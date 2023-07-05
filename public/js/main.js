e6d71dbc1b232e32f93bde43542c95692bb8ab41
sli6ct <sli6ct@bosch.com>
2023-07-05 15:40:25 -0300

    - Added search column dark mode; - Corrected map position;

--- public/js/main.js
+++ public/js/main.js
@@ -3,8 +3,10 @@ function darkMode() {
     var logo = document.getElementById("logo");
     var darkmodeBtn = document.getElementById("darkmode-btn");
     var footer = document.getElementById("footer");
+    var searchColumn = document.getElementById("searchColumn");
     element.classList.toggle("dark-mode");
     logo.classList.toggle("logo-dark");
     darkmodeBtn.classList.toggle("darkmode-btn-dark");
     footer.classList.toggle("footer-dark");
+    searchColumn.classList.toogle("searchColumn-dark")
  }

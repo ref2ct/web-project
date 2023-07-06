11f83a0480bd4b5cce9bfc0ef2ce9c2f6999b6c0
ref2ct <felipe.reimer@br.bosch.com>
2023-07-06 09:16:03 -0300

    FIX | Modal functionality

--- public/js/leaflet.js
+++ public/js/leaflet.js
@@ -4,6 +4,7 @@ L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'  
 }).addTo(map);  
 
+
 const geocoder = L.Control.Geocoder.nominatim();  
 const searchButton = document.getElementById('searchButton');  
 const searchInput = document.getElementById('searchInput');  

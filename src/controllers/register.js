af4bb9e02999046fa80ee1d290d6a1d132b75a50
KAE1CT <eric.kaletka@br.bosch.com>
2023-07-10 13:36:26 -0300

    alert message

--- src/controllers/register.js
+++ src/controllers/register.js
@@ -1,32 +1,29 @@
 const database = require('../config/db');
 const user = require('../model/user');
 
-module.exports = 
-{
-    async pagRegisterGet(req, res)
-    {
-        res.render('../views/register-view',{user:''});
+module.exports = {
+    async pagRegisterGet(req, res) {
+        res.render('../views/register-view', { user: '' });
     },
 
-    async userInsert(req, res)
-    {
+    async userInsert(req, res) {
         const dados = req.body;
 
-        await user.create
-        (
-            {
-                EDV: dados.edv,
-                Nome: dados.nome,
-                Rua: dados.rua,
-                Número: dados.numero,
-                Bairro: dados.bairro,
-                Telefone: dados.telefone,
-                Email: dados.email,
-                Horario: dados.horario
-            }
-        );
+        if (!dados.edv || !dados.nome || !dados.rua || !dados.numero || !dados.bairro || !dados.telefone || !dados.email || !dados.horario) {
+            return res.send('<script>alert("Usuário não cadastrado. Preencha todos os campos obrigatórios."); window.location.href = "/register";</script>');
+        }
 
-        res.redirect('/')
+        await user.create({
+            EDV: dados.edv,
+            Nome: dados.nome,
+            Rua: dados.rua,
+            Número: dados.numero,
+            Bairro: dados.bairro,
+            Telefone: dados.telefone,
+            Email: dados.email,
+            Horario: dados.horario
+        });
+
+        return res.send('<script>alert("Usuário cadastrado com sucesso."); window.location.href = "/";</script>');
     }
 }
-
